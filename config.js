61faccc36bebf89ced41d867dabd181cca2236fc
vaivory <vaivor@yahoo.com>
2022-06-13 19:32:31 +0300

    Cache Time

--- config.js
+++ config.js
@@ -11,6 +11,11 @@ config.dev = {
         password: 'admin',
         database: 'batai',
     },
+    cache: {
+        default: 0,
+        periods: {      
+        }
+    }
 }
 
 config.prod = {
@@ -24,6 +29,27 @@ config.prod = {
         password: 'r84tr5s25e84rrg52f5er84r5ert8r4g55e',
         database: 'batai-r5fe1d15',
     },
+    cache: {
+        default: 60*60,
+        periods: {
+            css: 60 * 60,
+            js: 60 * 60,
+            svg: 60 * 60,
+            png: 60 * 60,
+            jpg: 60 * 60,
+            ico: 60 * 60,
+            woff2: 60 * 60,
+            woff: 60 * 60,
+            ttf: 60 * 60,
+            otf: 60 * 60,
+            eot: 60 * 60,
+            webmanifest: 60 * 60,
+            pdf: 60 * 60,
+            json: 60 * 60,
+        }
+    }
+        
+    
 }
 
 const nodeEnv = process.env.NODE_ENV;

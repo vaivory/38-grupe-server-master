b62da54e3a67311ab741815c2e5e487eb278ebb6
vaivory <vaivor@yahoo.com>
2022-06-15 21:26:59 +0300

    css failiukai puslapiui

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,14 @@
 import { PageTemplate } from "../lib/PageTemplate.js";
 
 class Page404 extends PageTemplate {
+    constructor() {
+        super();
+        this.title = '404 | Server';
+    }
 
+    mainHTML() {
+        return `<h1>404 page 🎅</h1>`;
+    }
 }
 
-export { Page404 }; 
+export { Page404 };
